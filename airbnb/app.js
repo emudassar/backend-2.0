a3e2cc00ec3411428e23017e5e3361d64d94e1c8
M Mudassar <contacmudassar@gmail.com>
2026-01-12 13:34:57 +0500

    cleaning projects

--- airbnb/app.js
+++ airbnb/app.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
 const express = require('express')
+const app = express()
 
 const userRouter = require("./routes/userRouter")
 const hostRouter = require("./routes/hostRouter")
 const rootDir = require('./utils/pathUtil')
 
-const app = express()
+app.use(express.static(path.join(rootDir, 'public')))
 
 app.use(express.urlencoded())
 app.use(userRouter)
